c5728abe411f46cf31a17846d25c9dd905043594
mikhail-karpov <mikhailkarpov@hotmail.com>
2024-07-28 17:31:32 +0400

    add login redirect

--- resource-server/src/main/java/com/mikhailkarpov/security/SecurityConfig.java
+++ resource-server/src/main/java/com/mikhailkarpov/security/SecurityConfig.java
@@ -2,11 +2,11 @@ package com.mikhailkarpov.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
@@ -20,7 +20,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
         .cors(cors -> cors.configurationSource(configurationSource()))
         .csrf(AbstractHttpConfigurer::disable)
-        .oauth2Login(Customizer.withDefaults())
+        .oauth2Login(login -> login.successHandler(loginSuccessHandler()))
         .build();
   }
 
@@ -35,6 +35,11 @@ public class SecurityConfig {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", cors);
     return source;
+  }
+
+  @Bean
+  SimpleUrlAuthenticationSuccessHandler loginSuccessHandler() {
+    return new SimpleUrlAuthenticationSuccessHandler("http://localhost:5173");
   }
 
 }
