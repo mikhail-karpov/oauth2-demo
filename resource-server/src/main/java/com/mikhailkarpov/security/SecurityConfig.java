62d73814ff6b7a5c97fa4e19be1b3bf5ba380339
mikhail-karpov <mikhailkarpov@hotmail.com>
2024-07-28 16:48:03 +0400

    add CORS and CSRF config

--- resource-server/src/main/java/com/mikhailkarpov/security/SecurityConfig.java
+++ resource-server/src/main/java/com/mikhailkarpov/security/SecurityConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -15,8 +18,23 @@ public class SecurityConfig {
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
         .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        .cors(cors -> cors.configurationSource(configurationSource()))
+        .csrf(AbstractHttpConfigurer::disable)
         .oauth2Login(Customizer.withDefaults())
         .build();
+  }
+
+  @Bean
+  UrlBasedCorsConfigurationSource configurationSource() {
+    CorsConfiguration cors = new CorsConfiguration();
+    cors.addAllowedOrigin("http://localhost:5173");
+    cors.addAllowedHeader("*");
+    cors.addAllowedMethod("*");
+    cors.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", cors);
+    return source;
   }
 
 }
